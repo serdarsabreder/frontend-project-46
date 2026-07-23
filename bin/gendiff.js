f97ec7eb108518c26988fda27b9994b4e2b71396
serdar <serdarsabreder17@gmail.com>
2026-07-23 20:22:00 +0300

    feat: CLI with commander, help/version, default export genDiff (JS with type: module)

---
+++ bin/gendiff.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+import { program } from 'commander';
+import genDiff from '../src/index.js';
+
+program
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0')
+  .argument('<filepath1>', 'First file path')
+  .argument('<filepath2>', 'Second file path')
+  .parse();
+
---
+++ src/index.js
@@ -0,0 +1,6 @@
+const genDiff = (filepath1, filepath2) => {
+  return 'Difference will be calculated here.';
+};
+
+export default genDiff;
+
