f315806929c1b59fa951b4e38731c7dacc4ce982
serdar <serdarsabreder17@gmail.com>
2026-07-23 22:44:18 +0300

    feat: add -f/--format option to CLI as required by step 3

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,5 +8,14 @@ program
   .version('1.0.0')
   .argument('<filepath1>', 'First file path')
   .argument('<filepath2>', 'Second file path')
+  .option('-f, --format [type]', 'Output format (stylish, plain, json)')
   .parse();
+
+const options = program.opts();
+const filepath1 = program.args[0];
+const filepath2 = program.args[1];
+
+// Пока передаём format в функцию, даже если она его ещё не использует
+const result = genDiff(filepath1, filepath2, options.format);
+console.log(result);
 
